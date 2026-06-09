d9863712b3ed1cf7c155166a0567f82b9ba141e4
Adil Poyraz AKA <workintech.poyrazaka@gmail.com>
2026-06-09 10:52:14 +0300

    feat(corn): grains, water, ripe + diary day one

--- farm/corn.py
+++ farm/corn.py
@@ -1,5 +1,10 @@
 
 class Corn:
-    # YOUR CODE HERE
-    pass
+    def __init__(self):
+        self.grains = 0
 
+    def water(self):
+        self.grains += 10
+
+    def ripe(self):
+        return self.grains >= 15
